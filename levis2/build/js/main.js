f99ed0a2d0d759abdccb59dcf3b86e4731250694
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-11-03 17:18:07 +0200

    add levis2 pr

---
+++ levis2/build/js/main.js
@@ -0,0 +1,225 @@
+var landingFunctions = {
+	init: function() {
+		this.initLibraris()
+		// this.modals()
+		this.time()
+	}, 
+
+	initLibraris: function() {
+
+		$('[href*="#"]').on('click', function (e) {
+			var fixedOffset = 0;
+			var cardHeight = $("#card").outerHeight(false)
+			var windowHeight = $(window).height()
+	
+
+			$('html, body')
+				.stop()
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+			e.preventDefault();
+		});
+
+		// function cardSlider (selector) {
+		// 	var owl = $(selector + " .card__galary").owlCarousel({
+		// 		items: 1,
+		// 		margin: 100,
+		// 		dots: false,
+		// 		nav: true,
+		// 		loop: true,
+		// 	});
+	
+		// 	$(selector + " .card__foto").each(function() {
+		// 		$(this).click(function() {
+		// 			$(selector + " .card__foto").removeClass("active")
+		// 			var position = $(this).data("slide") - 1
+		// 			owl.trigger("to.owl.carousel", [position, 300])
+		// 			$(this).addClass("active")
+		// 		})
+		// 	})
+		// }
+	
+		// cardSlider(".card__1")
+		// cardSlider(".card__2")
+
+
+		// $(".galary__slider").owlCarousel({
+		// 	loop: true,
+		// 	nav : true,
+		// 	dots: true,
+		// 	dotsEach: true,
+		// 	items: 1,
+		// })
+
+		// $(".review__slider").owlCarousel({
+		// 	loop: true,
+		// 	nav : true,
+		// 	dots: false,
+		// 	items: 1,
+		// 	margin: 50,
+		// 	autoHeight: true,
+		// 	responsive:{
+		// 		0:{
+		// 			dots: true,
+		// 			// dotsEach: true,
+		// 		},
+		// 		701:{
+		// 			dots: false,
+		// 			dotsEach: false,
+		// 		},
+		// 	}
+		// })
+
+	
+		// AOS.init({
+		// 	disable : 'mobile',
+		// 	once: true,
+		// 	duration: 600,
+		// 	// offset : -200,
+		// });
+	
+		// $(window).resize(function() {
+		// 	AOS.refresh();
+		// })
+
+		$('[data-fancybox]').fancybox({
+			loop: true,
+			infobar: false,
+			animationEffect: false,
+			backFocus: false,
+			hash: false,
+		});
+
+		// $(".year").text(new Date().getFullYear())
+	},
+
+
+	modals: function() {
+		function modal() {
+			$(".add__review").click(function () {
+				$(".modal").addClass("active")
+			})
+	
+			function close() {
+				$(".modal").removeClass("active")
+			}
+	
+			$(".modal").click( function(e) {
+				var target = e.target;
+				if(target.classList.contains("modal__close")) {
+					close()
+				}
+				if(target.classList.contains("modal")) {
+					close()
+				}
+			})
+	
+			function readURL(input) {
+				if (input.files && input.files[0]) {
+					var reader = new FileReader();
+					reader.onload = function (e) {
+						$('.file img').attr('src', e.target.result).css("display", "block");
+					};
+					reader.readAsDataURL(input.files[0]);
+				}
+			}
+	
+			$(".modal .input__file").on("change", function () {
+				readURL(this);
+			});
+	
+			$(".modal form").submit(function (e) {
+				e.preventDefault()
+				$(this).removeClass("active");
+				$(".send__window").addClass("active");
+				$(".modal .name__input").val("")
+				$(".modal .modal__area").val("")
+				$(".modal .file img").attr("src", "").css("display", "none")
+				delayClose()
+			})
+			function delayClose() {
+				setTimeout(function () {
+					$(".modal form").addClass("active");
+					$(".send__window").removeClass("active");
+					close();
+				}, 5000);
+			}
+		}
+	
+		modal()
+	
+		function privacy() {
+			$(".confidantion").click(function () {
+				$(".privacy-policy-popup").addClass("active")
+			})
+	
+			function close() {
+				$(".privacy-policy-popup").removeClass("active")
+			}
+	
+			$(".privacy-policy-popup").click( function(e) {
+				var target = e.target;
+				if(target.classList.contains("privacy__close")) {
+					close()
+				}
+				if(target.classList.contains("privacy-policy-popup")) {
+					close()
+				}
+			})
+		}
+	
+		privacy()
+	},
+
+	time: function() {
+
+		Date.prototype.daysInMonth = function () {
+			return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+		};
+		
+		if (!String.prototype.padStart) {
+			String.prototype.padStart = function padStart(targetLength, padString) {
+				targetLength = targetLength >> 0; //truncate if number or convert non-number to 0;
+				padString = String((typeof padString !== 'undefined' ? padString : ' '));
+				if (this.length > targetLength) {
+					return String(this);
+				}
+				else {
+					targetLength = targetLength - this.length;
+					if (targetLength > padString.length) {
+						padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+					}
+					return padString.slice(0, targetLength) + String(this);
+				}
+			};
+		}
+
+		function getDate(plusDays) {
+			var today = new Date();
+			var dd = String(today.getDate() + plusDays).padStart(2, '0');
+			var mm = String(today.getMonth() + 1).padStart(2, '0');
+			if (+dd < 0) {
+				mm = String(today.getMonth()).padStart(2, '0');
+			}
+			
+			var yyyy = String(today.getFullYear());
+			// yyyy = yyyy.substr(yyyy.length - 2);
+			var currentDaysInMonth = new Date().daysInMonth()
+			if (+dd > currentDaysInMonth) {
+				dd = String(dd - currentDaysInMonth).padStart(2, '0');
+				mm++
+			}
+			if (+dd < 0) {
+				dd = String(currentDaysInMonth + +dd).padStart(2, '0');
+			}
+			return dd + "." + mm + "." + yyyy
+		}
+	
+		$(".discount__time span").text(getDate(2));
+	},
+	
+}
+
+$(document).ready(function() {
+	landingFunctions.init();
+});
+
