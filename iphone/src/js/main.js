ffa963f1999b8ba407984fa6b1daae8cd98d92b6
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-24 16:18:20 +0200

    add desktop

--- iphone/src/js/main.js
+++ iphone/src/js/main.js
@@ -2,7 +2,7 @@ var landingFunctions = {
 	init: function() {
 		this.initLibraris()
 		// this.time()
-		// this.modals()
+		this.modals()
 		this.card()
 	}, 
 
@@ -21,23 +21,15 @@ var landingFunctions = {
 			e.preventDefault();
 		});
 
-		// $(".review__slider").owlCarousel({
-		// 	loop: true,
-		// 	nav : true,
-		// 	dots: false,
-		// 	dotsEach: false,
-		// 	items: 2,
-		// 	margin: 100,
-		// 	// autoHeight: true,
-		// 	responsive:{
-		// 		0:{
-		// 			items:1,
-		// 		},
-		// 		1000:{
-		// 			items:2,
-		// 		}
-		// 	}
-		// });
+		$(".review__slider").owlCarousel({
+			loop: true,
+			nav : true,
+			dots: false,
+			dotsEach: false,
+			items: 1,
+			margin: 50,
+			// autoHeight: true,
+		});
 
 		$.raty.path = 'img/raty';
 
