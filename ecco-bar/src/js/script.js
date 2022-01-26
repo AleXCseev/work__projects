ae0bdae37358e23c0fbfafe3f6739380c38cf890
Alekseev Konstantin <alexcseev1990@gmail.com>
2022-01-26 17:21:17 +0200

    add desktop

--- ecco-bar/src/js/script.js
+++ ecco-bar/src/js/script.js
@@ -1,37 +1,44 @@
 var landingFunctions = {
 	init: function() {
 		this.initLibraris()
-		// this.card()
-		// this.time()
+		this.card()
+		this.time()
 		this.galary()
 		this.bar()
-		// this.paralax()
-		// this.video()
-		// this.modal()
+		this.modal()
+		this.header()
 	}, 
 
 	initLibraris: function() {
 
 		$('[href*="#"]').on('click', function (e) {
 			var fixedOffset = 0;
-			var cardHeight = $("#bar").outerHeight(false)
-			var windowHeight = $(window).height()
+			// var cardHeight = $("#bar").outerHeight(false)
+			// var windowHeight = $(window).height()
 	
 
 			$('html, body')
 				.stop()
-				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
-				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
 			e.preventDefault();
 		});
 
-		// $(".review__slider").owlCarousel({
-		// 	loop: true,
-		// 	nav : true,
-		// 	dots: false,
-		// 	items: 1,
-		// 	margin: 50,
-		// });
+		$.raty.path = 'img/raty';
+
+		$('.modal__raiting').raty({
+			half: false,
+			space: false,
+			number: 5,
+		});
+
+		$(".review__slider").owlCarousel({
+			loop: true,
+			nav : true,
+			dots: false,
+			items: 1,
+			margin: 50,
+		});
 
 		// AOS.init({
 		// 	disable : 'mobile',
@@ -44,6 +51,16 @@ var landingFunctions = {
 		// 	AOS.refresh();
 		// })
 
+		$('[data-fancybox]').fancybox({
+			loop: true,
+			infobar: false,
+			animationEffect: false,
+			backFocus: false,
+			hash: false,
+		});
+	},
+
+	header: function() {
 		function headerAnimate() {
 			$(".header__boot").each(function() {
 				if($(this).hasClass("boot__active")) {
@@ -61,15 +78,7 @@ var landingFunctions = {
 			})
 		}
 
-		setInterval(headerAnimate , 5000)
-
-		$('[data-fancybox]').fancybox({
-			loop: true,
-			infobar: false,
-			animationEffect: false,
-			backFocus: false,
-			hash: false,
-		});
+		setInterval(headerAnimate , 4000)
 	},
 
 	galary: function() {
@@ -93,6 +102,16 @@ var landingFunctions = {
 		}
 	
 		galary(".galary__wrapper")
+
+		if($(window).width() <= 1000) {
+			$(".galary").addClass("owl-carousel").owlCarousel({
+				loop: true,
+				nav : false,
+				dots: false,
+				items: 1,
+				margin: 50,
+			});
+		}
 	},
 
 	bar: function() {
@@ -125,49 +144,42 @@ var landingFunctions = {
 	},
 
 	card: function() {
-		$(".card__slider").owlCarousel({
-			loop: true,
-			nav : true,
-			dots: true,
-			dotsEach: true,
-			items: 1,
-			margin: 50,
-			// autoHeight: false,
-		})
-
-		function switchBtns(selector) {
-			$(selector + " .card__size-btn").click(function () {
-				$(selector + " .card__size-btn").removeClass("active")
-				$(this).addClass("active");
-			})
-		}
-	
-		switchBtns(".card__1");
-
 		function galary(selector) {
-			var galaryFototsSelector = selector + " .card__slide img";
+			var galaryFotosSelector = selector + " .card__foto-wrapper img";
+			var galaryMainFotosSelector = selector + " .card__boot";
 	
 			function toggleDataSrcAtribute(string) {
-				$(galaryFototsSelector).each(function () {
-					if($(this).attr("src") === $(this).attr("data-" + string)) {
-						return
-					}
-					$(this)
+				$(galaryFotosSelector).each(function () {
+					$(this).parent().hide()
+					$(this).parent().attr("href", $(this).attr("data-" + string))
+					$(this).attr("src",  $(this).attr("data-" + string))
+					$(this).parent().fadeIn(1000);
+
+					$(galaryMainFotosSelector)
 						.hide()
-						.attr("src",  $(this).attr("data-" + string))
-						.fadeIn(1000)
+						.attr("src",  $(galaryMainFotosSelector).attr("data-" + string))
+						.fadeIn(1000);
 				})
 			}
 	
-			$(selector + " .card__color-btn").click(function () {
-				$(selector + " .card__color-btn").removeClass("active")
+			$(selector + " .color__btn").click(function () {
+				if($(this).hasClass("active")) {
+					return false
+				}
+				
+				$(selector + " .color__btn").removeClass("active")
 				var color = $(this).data("color")
 				$(this).addClass("active")
 				toggleDataSrcAtribute(color)
+
+				var text = $(selector + " .card__title").attr("data-" + color)
+
+				$(selector + " .card__title").hide().text(text).fadeIn(1000);
+				// console.log(text)
 			})
 		}
-	
-		galary(".card__1")
+
+		galary(".card__2")
 	},
 
 	time: function() {
@@ -216,87 +228,9 @@ var landingFunctions = {
 		$(".date__1").text(getDate(-5));
     	$(".date__2").text(getDate(2));
 
-		$(".header__discount span").text(getDate(2));
+		// $(".header__discount span").text(getDate(2));
 
 		// $(".year").text(new Date().getFullYear())
-	},
-
-	paralax: function() {
-		var scenesParallax = [];
-
-		mQ("(max-width: 1023px)", function () {
-			if (!scenesParallax.length) return
-			scenesParallax.forEach(function (scene) {
-				scene.disable();
-				scene.element.removeAttribute('style');
-			})
-		}, 
-
-		function () {
-			if (scenesParallax.length === 0) {
-				$('.parallax').each(function (i) {
-					scenesParallax[i] = new Parallax($(this).children('div').attr('data-depth', randomNum(10, 20)).end().get(0), {
-						frictionX: 0.002,
-						frictionY: 0.002,
-						invertX: Math.random() >= 0.5,
-						invertY: Math.random() >= 0.5
-					});
-				})
-			} else {
-				scenesParallax.forEach(function (scene) {
-					scene.enable();
-				})
-			}
-		});
-	
-		function randomNum(min, max) {
-			var numLow = min, numHigh = max,
-			adjustedHigh = (parseFloat(numHigh) - parseFloat(numLow)) + 1;
-			return Math.floor(Math.random() * adjustedHigh) + parseFloat(numLow);
-		}
-	
-		function mQ(mqStr, match, mismatch) {
-			var mq = matchMedia(mqStr);
-			mq.addListener(widthChange);
-			widthChange(mq);
-			function widthChange(mq) {
-				if (mq.matches) {
-					match();
-				} else {
-					mismatch();
-				}
-			}
-		}
-	},
-
-	video: function() {
-		function addVideoOnPage(selector) {
-			var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera))||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera).substr(0,4));if (isMobile) {    var tag = document.createElement('script');    tag.src = "https://www.youtube.com/iframe_api";    var firstScriptTag = document.getElementsByTagName('script')[0];    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);}
-			$(selector).click(function() {
-				$(this).addClass("inactive")
-				if ($("iframe", this).length) {
-					return;
-				}
-				var videoId = $(this).data("video");
-				if (isMobile) {
-					var videoElId = "video-" + Date.now();
-					$(this).append("<div id='"+videoElId+"'></div>");
-					var player = new YT.Player(videoElId, {
-						videoId: videoId,
-						events: {
-							onReady: function() {
-								player.playVideo();
-							}
-						}
-					});
-				} else {
-					var videoSrc = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&mute=1";
-					$(this).append("<iframe src=\""+videoSrc+"\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>");
-				}
-			});
-		}
-	
-		addVideoOnPage(".video");	
 	},
 
 	modal: function() {
