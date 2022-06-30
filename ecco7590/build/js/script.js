727ec7c8c06d822e00a1b96c3d6261f2229f2b52
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-06-30 17:02:12 +0300

    fix

--- ecco7590/build/js/script.js
+++ ecco7590/build/js/script.js
@@ -25,14 +25,14 @@ var landingFunctions = {
 		}
 
 		$('[href*="#"]').on('click', function (e) {
-			var fixedOffset = 20;
-			var cardHeight = $("#card").outerHeight(false)
-			var windowHeight = $(window).height()
+			var fixedOffset = -20;
+			// var cardHeight = $("#card").outerHeight(false)
+			// var windowHeight = $(window).height()
 
 			$('html, body')
 				.stop()
-				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
-				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
 			e.preventDefault();
 		})
 
