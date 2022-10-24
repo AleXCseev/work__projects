4d1b50bb0b9ad6b2a540f53689d80a96d583dd9f
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-10-24 17:47:16 +0300

    add new pr

---
+++ gas/src/js/script.js
@@ -0,0 +1,240 @@
+var landingFunctions = {
+	init: function() {
+		this.initLibraris()
+		this.time()
+		this.modal()
+		this.quantity()
+	}, 
+
+	initLibraris: function() {
+
+		$('[href*="#"]').on('click', function (e) {
+			var fixedOffset = 0;
+			var cardHeight = $("#card").outerHeight(false)
+			var windowHeight = $(window).height()
+
+			$('html, body')
+				.stop()
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+			e.preventDefault();
+		});
+
+		// $(".card__slider").owlCarousel({
+		// 	items: 1,
+		// 	margin: 20,
+		// 	dots: true,
+		// 	nav: true,
+		// 	loop: true,
+		// });
+
+		$(".review__slider").owlCarousel({
+			items: 1,
+			margin: 20,
+			dots: true,
+			nav: true,
+			loop: true,
+			autoHeight: true,
+		});
+
+		$.raty.path = $("body").data("path") +  '/img/raty';
+
+		$('.modal__raiting').raty({
+			half: true,
+			space: false,
+			number: 5,
+		});
+
+		$(window).scroll(function() {
+			$('.header__reminder').each(function(){
+			var imagePos = $(this).offset().top;
+			
+	
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow + 1000) {
+					$(this).find(".header__reminder-line").addClass("reminder__animation");
+				}
+			});
+		});
+
+		// AOS.init({
+		// 	disable : 'mobile',
+		// 	once: true,
+		// 	duration: 1000,
+		// 	offset : -200,
+		// });
+	
+		// $(window).resize(function() {
+		// 	AOS.refresh();
+		// })
+
+		$('[data-fancybox]').fancybox({
+			loop: true,
+			infobar: false,
+			animationEffect: false,
+			backFocus: false,
+			hash: false,
+		});
+	},
+
+	time: function() {
+		Date.prototype.daysInMonth = function () {
+			return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+		};
+		
+		if (!String.prototype.padStart) {
+			String.prototype.padStart = function padStart(targetLength, padString) {
+				targetLength = targetLength >> 0; //truncate if number or convert non-number to 0;
+				padString = String((typeof padString !== 'undefined' ? padString : ' '));
+				if (this.length > targetLength) {
+					return String(this);
+				}
+				else {
+					targetLength = targetLength - this.length;
+					if (targetLength > padString.length) {
+						padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+					}
+					return padString.slice(0, targetLength) + String(this);
+				}
+			};
+		}
+
+		function timer () {
+			function runMultiple(hoursSelector, minutesSelector, secondsSelector, milisecondsSelector) {
+				var d = new Date();
+				var h = String(23 - d.getHours()).padStart(2, "0");
+				var m = String(59 - d.getMinutes()).padStart(2, "0");
+				var s = String(60 - d.getSeconds()).padStart(2, "0");
+				// var ms = String(1000 - d.getMilliseconds()).padStart(3, "0");
+				$(hoursSelector).text(h)
+				$(minutesSelector).text(m)
+				$(secondsSelector).text(s)
+				// $(milisecondsSelector).text(ms)
+			}
+			setInterval(function () {
+				runMultiple(".hours", ".minutes", ".seconds")
+			}, 1000);
+		}
+	
+		timer()
+
+		function getDate(plusDays) {
+			var today = new Date();
+			var dd = String(today.getDate() + plusDays).padStart(2, '0');
+			var mm = String(today.getMonth() + 1).padStart(2, '0');
+			if (+dd < 0) {
+				mm = String(today.getMonth()).padStart(2, '0');
+			}
+			
+			var yyyy = String(today.getFullYear());
+			// yyyy = yyyy.substr(yyyy.length - 2);
+			var currentDaysInMonth = new Date().daysInMonth()
+			if (+dd > currentDaysInMonth) {
+				dd = String(dd - currentDaysInMonth).padStart(2, '0');
+				mm = String(+mm + 1).padStart(2, '0');
+			}
+			if (+dd < 0) {
+				dd = String(currentDaysInMonth + +dd).padStart(2, '0');
+			}
+			if (+dd == 0) {
+				dd = "01"
+			}
+			return dd + "." + mm + "." + yyyy
+		}
+
+		// $(".date__1").text(getDate(-5));
+    	// $(".date__2").text(getDate(2));
+
+		$(".card__date span").text(getDate(2))
+	},
+
+	modal: function() {
+		function modal() {
+			$(".add__review").click(function () {
+				$(".modal__review").addClass("active")
+			})
+	
+			function close() {
+				$(".modal__review").removeClass("active")
+			}
+	
+			$(".modal__review").click( function(e) {
+				var target = e.target;
+				if(target.classList.contains("modal__close")) {
+					close()
+				}
+				if(target.classList.contains("modal")) {
+					close()
+				}
+			})
+	
+			function readURL(input) {
+				if (input.files && input.files[0]) {
+					var reader = new FileReader();
+					console.log(reader)
+					reader.onload = function (e) {
+						$('.file img').attr('src', e.target.result).css("display", "block");
+					};
+					reader.readAsDataURL(input.files[0]);
+				}
+			}
+	
+			$(".modal__review .input__file").on("change", function () {
+				readURL(this);
+			});
+	
+			$(".modal__review form").submit(function (e) {
+				e.preventDefault()
+				$(this).removeClass("active");
+				$(".send__window").addClass("active");
+				$(".modal__review .name__input").val("")
+				$(".modal__review .modal__area").val("")
+				$(".modal__review .file img").attr("src", "").css("display", "none")
+				delayClose()
+			})
+			function delayClose() {
+				setTimeout(function () {
+					$(".modal__review form").addClass("active");
+					$(".send__window").removeClass("active");
+					close();
+				}, 5000);
+			}
+		}
+	
+		modal()
+	},
+
+	quantity: function() {
+		var currentNumber;
+		var prefix = $(".quantity").data("prefix")
+
+		function getRandomInt(max) {
+			return Math.floor(Math.random() * Math.floor(max));
+		}
+
+		if(localStorage.getItem("quantity")) {
+			$(".quantity").text(localStorage.getItem("quantity"));
+		} else {
+			currentNumber = 25
+			localStorage.setItem("quantity", currentNumber)
+			$(".quantity").text(currentNumber);
+		}
+
+		setInterval(function () {
+			currentNumber = localStorage.getItem("quantity");
+			if (currentNumber >= 3) {
+				currentNumber = currentNumber - getRandomInt(3);
+				$(".quantity").text(currentNumber);
+				localStorage.setItem("quantity", currentNumber)
+			} else {
+				currentNumber = 25;
+				localStorage.setItem("quantity", currentNumber)
+			}
+		}, 100000)
+	},
+}
+
+$(document).ready(function() {
+	landingFunctions.init();
+});
+
